535d6631605f6f28a61e59c7edaf8cc22d0a499b
shylajaam <155047839+shylajaam@users.noreply.github.com>
2023-12-29 15:06:23 +0530

    Initial commit

---
+++ javac string.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListProcessor {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("banana");
+        words.add("orange");
+
+        // Task: Use Copilot to complete the code to print each word in uppercase
+        for (String word : words) {
+
+
+
+         String uppercaseWord = word.toUpperCase();
+            System.out.println(uppercaseWord);
+}
+}
+}
---
+++ t2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class SortingExample {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 1, 7};
+
+        // Sorting the array
+        Arrays.sort(numbers);
+
+        // Printing the sorted array
+        System.out.println("Sorted numbers: " + Arrays.toString(numbers));
+    }
+}
